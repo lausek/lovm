71b0b5eaff20fb7463a03be832909219b8938414
lausek <root@lausek.eu>
2019-03-23 16:47:44 +0100

    running files

---
+++ vm/src/bin.rs
@@ -0,0 +1,21 @@
+extern crate lovm_asm_lib;
+
+use lovm::*;
+use lovm_asm_lib::*;
+
+use std::env;
+use std::io::Read;
+
+fn main() {
+    let mut args = env::args().skip(1);
+    let program = args.next().expect("no program specified");
+
+    let mut vm = vm::Vm::new();
+
+    let mut file = std::fs::File::open(program).expect("cannot read file");
+    let mut src = String::new();
+    file.read_to_string(&mut src).expect("reading file failed");
+    let codeblock = compiler::compile(src.as_ref()).unwrap();
+
+    vm.run(&codeblock).unwrap();
+}
